177c5084fe5f79449c9dc0827afaa52a3e643961
Art D <adpm@list.ru>
2023-01-27 02:27:28 +0600

    Add change locale

--- app/src/main/java/com/example/weatherapp/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/MainScreen.kt
@@ -1,41 +1,51 @@
 package com.example.weatherapp.ui.screens
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.res.Configuration
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.Button
-import androidx.compose.material.Checkbox
-import androidx.compose.material.Icon
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.example.weatherapp.R
 import com.example.weatherapp.viewmodel.MainViewModel
 import org.koin.androidx.compose.koinViewModel
+import java.util.*
 
 @Composable
 fun MainScreen(
-    vm : MainViewModel = koinViewModel()
+    vm: MainViewModel = koinViewModel()
 ) {
-    val checkedState = remember { mutableStateOf(false) }
+    val checkedStateRu = remember { mutableStateOf(false) }
+    val checkedStateUsa = remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    val config = LocalConfiguration.current
+    val resources = LocalContext.current.resources
+
+    Log.d("TAG-MAIN SCREEN", config.locales.toString())
+
     Surface {
         Column(
 
         ) {
             Text(
-                text = "Текст с информацией о текущей температуре"
+                text = stringResource(R.string.temperature)
             )
             Text(
-                text = "Текст с информацией о городе"
+                text = stringResource(R.string.city)
             )
             Text(
-                text = "Текст с информацией, когда было последнее обновление данных"
+                text = stringResource(R.string.last_update)
             )
             Icon(
                 imageVector = Icons.Filled.Home,
@@ -43,18 +53,29 @@ fun MainScreen(
             )
             Button(
                 onClick = {
+                    context.createConfigurationContext(context.resources.configuration)
+                    context.findActivity()?.recreate()
                     Log.d("TAG-MAINSCREEN", "button clicked")
                 }
             ) {
-                Text(text = "Обновить")
+                Text(text = stringResource(R.string.button_update))
             }
             Row {
                 Checkbox(
-                    checked = checkedState.value,
-                    onCheckedChange = { checkedState.value = it },
+                    checked = checkedStateUsa.value,
+                    onCheckedChange = {
+                        config.setLocale(Locale("en"))
+                        resources.updateConfiguration(
+                            context.resources.configuration,
+                            resources.displayMetrics
+                        )
+                        checkedStateUsa.value = it
+                        checkedStateRu.value = false
+
+
+                    },
                 )
                 Icon(
-//                    imageVector = ImageVector.vectorResource(id = R.drawable.flag_united_states),
                     painter = painterResource(R.drawable.flag_usa),
                     contentDescription = "Кнопка с иконкой США",
                     tint = Color.Unspecified
@@ -64,11 +85,19 @@ fun MainScreen(
             }
             Row {
                 Checkbox(
-                    checked = checkedState.value,
-                    onCheckedChange = { checkedState.value = it },
+                    checked = checkedStateRu.value,
+                    onCheckedChange = {
+                        config.setLocale(Locale("ru"))
+                        resources.updateConfiguration(
+                            context.resources.configuration,
+                            resources.displayMetrics
+                        )
+                        checkedStateRu.value = it
+                        checkedStateUsa.value = false
+                    },
                 )
                 Icon(
-                    painter = painterResource( R.drawable.flag_russia),
+                    painter = painterResource(R.drawable.flag_russia),
                     contentDescription = "Кнопка с иконкой России",
                     tint = Color.Unspecified
                 )
@@ -76,4 +105,10 @@ fun MainScreen(
 
         }
     }
+}
+
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
 }
